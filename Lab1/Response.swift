dbe478e71f4c847e511bbb3414e454c09abb16d0
Anna <anytkin11@gmail.com>
2020-11-29 19:22:35 +0200

    Initial commit

---
+++ Lab1/Response.swift
@@ -0,0 +1,13 @@
+//
+//  Response.swift
+//  Lab1
+//
+//  Created by Lena Soroka on 24.11.2020.
+//  Copyright © 2020 Anna Berezhniak. All rights reserved.
+//
+
+import Foundation
+
+class Response: Codable {
+    let Search: [Movie]
+}
